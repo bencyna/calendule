d7aaefcfda1d17c4fcb6546e0a85dd1681ab3568
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2021-05-30 16:41:19 +0930

    change icon

---
+++ client/src/components/TimePicker/index.js
@@ -0,0 +1,15 @@
+// import React, { useState } from "react";
+// import LuxonUtils from "@date-io/luxon";
+// import { TimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+
+// export default function TimeInput() {
+//   const [selectedDate, handleDateChange] = useState(new Date());
+
+//   return (
+//     <div>
+//       <MuiPickersUtilsProvider utils={LuxonUtils}>
+//         <TimePicker value={selectedDate} onChange={handleDateChange} />
+//       </MuiPickersUtilsProvider>
+//     </div>
+//   );
+// }
